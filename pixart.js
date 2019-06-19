e9918103df1516ebf2b0611976640e85ef8da0f7
Tiffany Mitchell <mitchelltiffany0@gmail.com>
2019-06-19 12:29:55 -0400

    add 20 divs to js

--- pixart.js
+++ pixart.js
@@ -1,5 +1,15 @@
 let submitBtn = document.querySelector('#set-color');
+let body = document.querySelector('body');
 
 submitBtn.addEventListener('click', function(evt) {
     // prevent default so page does not refresh
     evt.preventDefault();
+}
+    )    
+    
+    for(let i = 0; i < 20; i++) {
+        let div = document.createElement('div');
+        div.classList.add('square');
+        body.appendChild(div);
+    }
+    console.log(submitBtn)
