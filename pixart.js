7117c47524df73e8f3b78cb536415c890f0734e8
Tiffany Mitchell <mitchelltiffany0@gmail.com>
2019-06-19 12:55:40 -0400

    change square to green

--- pixart.js
+++ pixart.js
@@ -1,15 +1,23 @@
 let submitBtn = document.querySelector('#set-color');
 let body = document.querySelector('body');
+let colorVal = "green";
 
 submitBtn.addEventListener('click', function(evt) {
     // prevent default so page does not refresh
     evt.preventDefault();
-}
-    )    
-    
-    for(let i = 0; i < 20; i++) {
-        let div = document.createElement('div');
-        div.classList.add('square');
-        body.appendChild(div);
+
+    colorVal = document.querySelector('#color-field').value;
+    document.querySelector('.brush').style.backgroundColor = colorVal;
+})
+
+body.addEventListener('mouseover', function(evt) {
+    if(evt.target.classList.contains('square')) {
+        evt.target.style.backgroundColor = colorVal;
     }
-    console.log(submitBtn)
+})
+
+for(let i = 0; i < 8000; i++) {
+    let div = document.createElement('div');
+    div.classList.add('square');
+    body.appendChild(div);
+}
