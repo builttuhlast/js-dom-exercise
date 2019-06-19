78db0ec491c4eda208c08a726e774e30bfdb3253
Tiffany Mitchell <mitchelltiffany0@gmail.com>
2019-06-19 12:21:33 -0400

    set color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,5 @@
+let submitBtn = document.querySelector('#set-color');
 
+submitBtn.addEventListener('click', function(evt) {
+    // prevent default so page does not refresh
+    evt.preventDefault();
